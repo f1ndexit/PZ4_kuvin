5d268ee5f0a018e63047669e0c7e5f2429eb0936
Kuvin V.S <2010.kuvin@mail.ru>
2023-02-02 20:55:39 +0300

    Добавьте файлы проекта.

---
+++ PZ4_kuvin/StackPanel_2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PZ4_kuvin
+{
+    /// <summary>
+    /// Логика взаимодействия для StackPanel_2.xaml
+    /// </summary>
+    public partial class StackPanel_2 : Window
+    {
+        public StackPanel_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
